6f4d72e9ee6ee3d45d3ec74a6640e770c6364c75
André Macedo <asilvamacedo@gmail.com>
2022-11-08 09:52:12 +0000

    express quest 5

--- usersHandlers.js
+++ usersHandlers.js
@@ -70,12 +70,31 @@ const getUsersById = (req, res) => {
       })
       .catch((err) => {
         console.error(err);
-        res.status(500).send("Error editing the movie");
+        res.status(500).send("Error editing the user");
+      });
+  };
+  const deleteUsers = (req, res) => {
+    const id = parseInt(req.params.id);
+      database
+      .query(
+        "delete from users where id= ?", [id]
+      )
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.status(404).send("Not Found");
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error deleting the user");
       });
   };
   module.exports = {
     getUsers,
     getUsersById,
     postUsers,
-    updateUsers
+    updateUsers,
+    deleteUsers
   }
