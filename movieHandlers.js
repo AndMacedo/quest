28b55f2d0ebc9704db15caebdc4767ede94f37b7
André Macedo <asilvamacedo@gmail.com>
2022-11-08 11:38:59 +0000

    expres quest 6

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,17 +1,5 @@
 const database = require("./database");
 
-const getMovies = (req, res) => {
-  database
-    .query("select * from movies")
-    .then(([movies]) => {
-      res.json(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error retrieving data from database");
-    });
-};
-
 const movies = [
   {
     id: 1,
@@ -38,46 +26,64 @@ const movies = [
     duration: 180,
   },
 ];
+
 /* 
 const getMovies = (req, res) => {
   res.json(movies);
 };
- */
+*/
 /* const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
-
+  
   const movie = movies.find((movie) => movie.id === id);
-
+  
   if (movie != null) {
     res.json(movie);
   } else {
     res.status(404).send("Not Found");
   }
 }; */
+const getMovies = (req, res) => {
+  let sql="select * from movies"
+  const sqlValues=[]
+
+  if (req.query.color != null) {
+    sql += " where color = ?";  
+    sqlValues.push(req.query.color);
+  
+    if (req.query.max_duration != null) {  
+    sql += " and duration <= ?";  
+    sqlValues.push(req.query.max_duration);  
+    }  
+  } else if (req.query.max_duration != null) {  
+    sql += " where duration <= ?";  
+    sqlValues.push(req.query.max_duration);  
+  }
+  database
+    .query(sql,sqlValues)
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
     .query("select * from movies where id = ?", [id])
     .then(([movies]) => {
-
       if (movies[0] != null) {
-
         res.json(movies[0]);
-
       } else {
-
         res.status(404).send("Not Found");
-
       }
-
     })
     .catch((err) => {
-
       console.error(err);
-
       res.status(500).send("Error retrieving data from database");
-
     });
 };
 const postMovie=(req,res)=>{
--- usersHandlers.js
+++ usersHandlers.js
@@ -1,8 +1,23 @@
 const database = require("./database");
 
 const getUsers = (req, res) => {
+    let sql = "select * from users"
+    const sqlValues =[]
+    if(req.query.language !=null){
+        sql += " where language = ?"
+        sqlValues.push(req.query.language)
+
+        if(req.query.city !=null){
+            sql += " and city = ?"
+            sqlValues.push (req.query.city)
+        }
+    }
+    else if(req.query.city !=null){
+        sql += " where city = ?"
+        sqlValues.push(req.query.city)
+    }
   database
-    .query("select * from users")
+    .query(sql,sqlValues)
     .then(([users]) => {
       res.json(users);
     })
@@ -16,26 +31,17 @@ const getUsersById = (req, res) => {
   
     database
       .query("select * from users where id = ?", [id])
-      .then(([users]) => {
-  
-        if (users[0] != null) {
-  
-          res.json(users[0]);
-  
+      .then(([users]) => {  
+        if (users[0] != null) {  
+          res.json(users[0]);  
         } else {
-  
-          res.status(404).send("Not Found");
-  
-        }
-  
+            res.status(404).send("Not Found");  
+        }  
       })
       .catch((err) => {
-  
-        console.error(err);
-  
+          console.error(err);  
         res.status(500).send("Error retrieving data from database");
-  
-      });
+    });
   };
   
   const postUsers=(req,res)=>{
